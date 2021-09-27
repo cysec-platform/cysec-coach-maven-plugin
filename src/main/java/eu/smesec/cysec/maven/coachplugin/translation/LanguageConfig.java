a27aa4d8ca340aee90804d8f0b1ec577e13515e5
Matthias Luppi <53262016+mluppi@users.noreply.github.com>
2021-09-27 16:22:26 +0200

    Rename packages/artifact and apply license

---
+++ src/main/java/eu/smesec/cysec/maven/coachplugin/translation/LanguageConfig.java
@@ -0,0 +1,55 @@
+/*-
+ * #%L
+ * Coach Maven Plugin
+ * %%
+ * Copyright (C) 2021 FHNW (University of Applied Sciences and Arts Northwestern Switzerland)
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package eu.smesec.cysec.maven.coachplugin.translation;
+
+/**
+ * Represents a single language configuration
+ *
+ * @author Matthias Luppi
+ */
+public class LanguageConfig {
+
+    private final String code;
+    private final boolean merge;
+    private final boolean flatify;
+
+    public LanguageConfig(final String code, final boolean merge, final boolean flatify) {
+        this.code = code;
+        this.merge = merge;
+        this.flatify = flatify;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public boolean isMerge() {
+        return merge;
+    }
+
+    public boolean isFlatify() {
+        return flatify;
+    }
+
+    @Override
+    public String toString() {
+        return "{code=" + code + ", " + "merge=" + merge + ", flatify=" + flatify + "}";
+    }
+}
